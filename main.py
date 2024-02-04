9dd911678e20aefae1c04c0fa90264d377a6cca6
Sylvernity <44215344+Sylvernity@users.noreply.github.com>
2024-02-04 13:05:28 -0500

    - Moved slash commands to separate file - Added md5sum hashing function

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 # This example requires the 'message_content' intent.
 import asyncio
 
-import discord
-from discord import app_commands
-from dotenv import load_dotenv
 import os
 import random
+from slashCommands import *
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-# Slash Command
-tree = app_commands.CommandTree(client)
-servers = [discord.Object(id=1160247666870075422)]
 
 
 async def presence():
@@ -44,30 +33,6 @@ async def presence():
         status = random.choice(presence_states)
         await client.change_presence(activity=status)
         await asyncio.sleep(3600)
-
-# Ping Command
-@tree.command(name="ping", description="Tests whether bot is online", guilds=servers)
-async def ping(interaction):
-    if interaction.user.id == 176106602833772544:
-        await interaction.response.send_message("Pong!")
-    else:
-        await interaction.response.send_message("ERROR: Incorrect User", ephemeral=True)
-
-# Add Command
-@tree.command(name="add", description="Adds two numbers", guilds=servers)
-async def add(interaction, first_num: int, second_num: int):
-    await interaction.response.send_message(first_num + second_num)
-
-# Command to send secret messages
-@tree.command(name="secret_message", description="This message will self-destruct in 5 seconds", guilds=servers)
-async def secret(interaction, message: str):
-    await interaction.response.send_message("*This message will self destruct in 5 seconds:*\n" + message)
-    interMessage = await interaction.original_response()
-    for i in range(1, 5):
-        await asyncio.sleep(1)
-        await interMessage.edit(content="*This message will self destruct in " + str(5 - i) + " seconds:*\n" + message)
-    await interMessage.edit(content="This message has self-destructed.")
-
 
 # Run when bot is ready to run
 @client.event
---
+++ slashCommands.py
@@ -0,0 +1,45 @@
+# Functions to perform various cyber commands
+
+import discord
+from discord import app_commands
+from dotenv import load_dotenv
+import hashlib
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+# Slash Command
+tree = app_commands.CommandTree(client)
+servers = [discord.Object(id=1160247666870075422)]
+
+# Ping Command
+@tree.command(name="ping", description="Tests whether bot is online", guilds=servers)
+async def ping(interaction):
+    if interaction.user.id == 176106602833772544:
+        await interaction.response.send_message("Pong!")
+    else:
+        await interaction.response.send_message("ERROR: Incorrect User", ephemeral=True)
+
+# Add Command
+@tree.command(name="add", description="Adds two numbers", guilds=servers)
+async def add(interaction, first_num: int, second_num: int):
+    await interaction.response.send_message(first_num + second_num)
+
+# Command to send secret messages
+@tree.command(name="secret_message", description="This message will self-destruct in 5 seconds", guilds=servers)
+async def secret(interaction, message: str):
+    await interaction.response.send_message("*This message will self destruct in 5 seconds:*\n" + message)
+    interMessage = await interaction.original_response()
+    for i in range(1, 5):
+        await asyncio.sleep(1)
+        await interMessage.edit(content="*This message will self destruct in " + str(5 - i) + " seconds:*\n" + message)
+    await interMessage.edit(content="This message has self-destructed.")
+
+# Command for MD5 hash
+@tree.command(name="md5sum", description="Hash using md5", guilds=servers)
+async def md5(interaction, message: str):
+    message = message.encode()
+    md5sum = hashlib.md5(message).hexdigest()
+    await interaction.response.send_message("Here is your hashed message: \n" + md5sum)
