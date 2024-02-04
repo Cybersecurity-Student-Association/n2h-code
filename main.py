facd112b55aa2cff75745a0cc75fc7695ebc6334
Sylvernity <44215344+Sylvernity@users.noreply.github.com>
2024-02-04 12:48:56 -0500

    - Added secret message function - Added active presence

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # This example requires the 'message_content' intent.
+import asyncio
 
 import discord
 from discord import app_commands
 from dotenv import load_dotenv
 import os
+import random
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -16,6 +18,32 @@ client = discord.Client(intents=intents)
 # Slash Command
 tree = app_commands.CommandTree(client)
 servers = [discord.Object(id=1160247666870075422)]
+
+
+async def presence():
+    await client.wait_until_ready()
+
+    presence_states = [
+        discord.Activity(type=discord.ActivityType.watching, name='John Hammond'),
+        discord.Activity(type=discord.ActivityType.watching, name='IT Crowd'),
+        discord.Activity(type=discord.ActivityType.watching, name='War Games'),
+        discord.Activity(type=discord.ActivityType.watching, name='HackerSploit'),
+        discord.Activity(type=discord.ActivityType.watching, name='IPSec'),
+        discord.Activity(type=discord.ActivityType.watching, name='Hak5'),
+        discord.Activity(type=discord.ActivityType.playing, name='on your network'),
+        discord.Activity(type=discord.ActivityType.playing, name='TCM Academy'),
+        discord.Activity(type=discord.ActivityType.playing, name='picoCTF'),
+        discord.Activity(type=discord.ActivityType.playing, name='Hack The Box'),
+        discord.Activity(type=discord.ActivityType.playing, name='TryHackMe'),
+        discord.Activity(type=discord.ActivityType.watching, name='W4nna Cry?'),
+        discord.Activity(type=discord.ActivityType.listening, name='Darknet Diaries'),
+        discord.CustomActivity(name='Preparing for CyberForge'),
+    ]
+
+    while not client.is_closed():
+        status = random.choice(presence_states)
+        await client.change_presence(activity=status)
+        await asyncio.sleep(3600)
 
 # Ping Command
 @tree.command(name="ping", description="Tests whether bot is online", guilds=servers)
@@ -30,10 +58,22 @@ async def ping(interaction):
 async def add(interaction, first_num: int, second_num: int):
     await interaction.response.send_message(first_num + second_num)
 
+# Command to send secret messages
+@tree.command(name="secret_message", description="This message will self-destruct in 5 seconds", guilds=servers)
+async def secret(interaction, message: str):
+    await interaction.response.send_message("*This message will self destruct in 5 seconds:*\n" + message)
+    interMessage = await interaction.original_response()
+    for i in range(1, 5):
+        await asyncio.sleep(1)
+        await interMessage.edit(content="*This message will self destruct in " + str(5 - i) + " seconds:*\n" + message)
+    await interMessage.edit(content="This message has self-destructed.")
 
+
+# Run when bot is ready to run
 @client.event
 async def on_ready():
     print(f"Logged in.")
+    client.loop.create_task(presence())
     await tree.sync(guild=discord.Object(id=1160247666870075422))
 
 # Test command with prefix
